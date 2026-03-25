f9bd8587542bd4949ca6dbc5120dc7197fc486a6
FaizRazi <91615321+FaizRazi@users.noreply.github.com>
2026-03-25 20:17:02 +0700

    Change frame format to RGB for YOLO detection
    Updated the frame format from BGR to RGB for YOLO detection and adjusted the annotated image conversion.

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import cv2
 from ultralytics import YOLO
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import av
 
 st.title("🔥 YOLOv8 Realtime Webcam (Streamlit)")
 
-# Load model (cache biar cepat)
 @st.cache_resource
 def load_model():
     return YOLO("yolov8n.pt")
@@ -14,26 +12,18 @@ def load_model():
 model = load_model()
 
 
-# ==============================
-# VIDEO TRANSFORMER (REALTIME)
-# ==============================
 class YOLOVideoTransformer(VideoTransformerBase):
     def transform(self, frame):
-        # Ambil frame dari webcam
-        img = frame.to_ndarray(format="bgr24")
+        img = frame.to_ndarray(format="rgb24")
 
-        # YOLO detection
         results = model(img, conf=0.3)
 
-        # Gambar bounding box
         annotated = results[0].plot()
+        annotated = annotated[:, :, ::-1]  # convert ke RGB
 
         return annotated
 
 
-# ==============================
-# STREAM WEBCAM
-# ==============================
 webrtc_streamer(
     key="yolo",
     video_transformer_factory=YOLOVideoTransformer
